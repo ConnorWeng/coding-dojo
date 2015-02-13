23261511e491097cc84fb5c7647ad93e3e52e892
Connor Weng <foamtea30@gmail.com>
2015-02-13 09:33:13 +0800

    change title to CodingDojo

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'tddd' });
+  res.render('index', { title: 'CodingDojo' });
 });
 
 router.get('/templates/:name', function(req, res) {
-  res.render(req.params.name, { title: 'tddd' });
+  res.render(req.params.name, { title: 'CodingDojo' });
 });
 
 module.exports = router;
