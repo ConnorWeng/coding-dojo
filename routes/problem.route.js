1785a642a23ddb60cdfad46e90b40b10e32ca86e
Connor Weng <foamtea30@gmail.com>
2015-04-13 13:34:19 +0800

    add problem id to problems zset as well

--- routes/problem.route.js
+++ routes/problem.route.js
@@ -6,7 +6,7 @@ router.post('/', addProblem);
 
 function addProblem(req, res, next) {
   db.addProblem(req.body.title, req.body.desc).then(function(reply) {
-    res.send(reply);
+    res.send('ok');
   }, function(reason) {
     next(reason);
   });
